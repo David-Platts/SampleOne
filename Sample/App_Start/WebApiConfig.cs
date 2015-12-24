39346b01cdcfbc1c4bf101963437545abf689522
David Morrow <david.morrow@platts.com>
2015-12-24 10:59:33 -0700

    Added some json config

--- Sample/App_Start/WebApiConfig.cs
+++ Sample/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Sample
 {
@@ -19,6 +21,14 @@ namespace Sample
 				routeTemplate: "api/{controller}/{id}",
 				defaults: new { id = RouteParameter.Optional }
 			);
+
+			//	Enable some "prettier" formatted json output - useful in browsers and some testing
+			var jsonSettings = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+			jsonSettings.SerializerSettings.Formatting = Formatting.Indented;
+
+			//	Note Angular and other javascript based code needs camel case as javascript is case sensitive
+			jsonSettings.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
 		}
 	}
 }
